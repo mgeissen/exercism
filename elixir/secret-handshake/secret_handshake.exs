9f89529a7e4223847cde051f5aa99c52067e5996
Matthias Geissendoerfer <matthias.geissendoerfer@hermes-ws.com>
2017-08-21 09:58:38 +0200

    added first code for solution

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -15,6 +15,19 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    reverse = false
+    if code >= 16, do: reverse = true
+
   end
+
+  def getCodeForBinary(position) do
+    return = ""
+        if code == 1, do: return = "wink"
+        if code == 2, do: return = "double blink"
+        if code == 3, do: return = "close your eyes"
+        if code == 4, do: return = "jump"
+        return
+  end
+
 end
 
